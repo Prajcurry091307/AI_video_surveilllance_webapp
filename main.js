7b37428d5d556c99dcbd897ac29aaf8fad051185
Prajcurry091307 <77421995+Prajcurry091307@users.noreply.github.com>
2021-12-07 16:54:43 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -33,7 +33,7 @@ function draw()
              text(objects[i].label +" " + percent +"%", objects[i].x+15, objects[i].y+15);
              noFill();
              stroke("#FF0000");
-             rect(objects[i].x, objects[i].y, objects[i].width, objects.height);
+             rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
             }
         }
 }
